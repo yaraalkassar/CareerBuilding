eb1fc3d5ff3c6da9d9903e79106abdff4929585e
Yara <yara.sarmad@yahoo.com>
2021-01-09 20:28:07 +0300

    added navbar navigation

--- careerbuilding/src/Components/Navbar/Navbar.js
+++ careerbuilding/src/Components/Navbar/Navbar.js
@@ -10,8 +10,21 @@ export default function Navbar() {
       <nav className="relative flex flex-wrap items-center justify-between px-4 lg:py-6 py-4 navbar-expand-lg">
         <div>
           <Link to="./">
-            <img src={logo} className="w-16 h-16" alt="logo" />
+            <img src={logo} className="" alt="logo" />
           </Link>
+        </div>
+        <div className="flex justify-between content-between">
+        <Link to="./"><p className="px-2 ">{"Home"}</p></Link>
+        <Link to="./"><p className="px-2 ">{"Vacancies"}</p></Link>
+        <Link to="./"><p className="px-2 ">{"Contact Us"}</p></Link>
+        <Link to="./"><p className="px-2 ">{"About Us"}</p></Link>
+        </div>
+        <div>
+        <Link>
+        <button className="bg-black text-white px-2 py-4">
+        {"Join"}
+        </button>
+        </Link>
         </div>
       </nav>
     </>
