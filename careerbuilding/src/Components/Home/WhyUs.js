88f9b8757ce64afc4fbaf65aa108723b15297b27
Yara <yara.sarmad@yahoo.com>
2021-01-12 22:22:17 +0300

    style changes

--- careerbuilding/src/Components/Home/WhyUs.js
+++ careerbuilding/src/Components/Home/WhyUs.js
@@ -1,39 +1,39 @@
 const WhyUs = () => {
-    return (
-        <div className="bg-darkBlue font-bold text-2xl text-white"> 
-            <h1> WHY CAREER BUILDING?</h1>
-           
-    <div className="w-full flex flex-col flex-wrap md:flex-row md:justify-around mx-auto h-full py-12 items-stretch">
-      <div className="w-2/3 lg:1/3 xl:w-1/5 rounded-lg bg-darkP shadow-lg">
-        <div className="px-4 py-16 bg-paleYellow shadow rounded ">
-          <p className="text-darkerBlue text-3xl mb-8" >Easy Apply</p>
-          <p className=" text-base text-darkerBlue ">
-            {('you can apply easy by one click!')}
-          </p>
+  return (
+    <div className="bg-darkBlue font-bold text-2xl text-white pt-8">
+      <h1> WHY CAREER BUILDING?</h1>
+      <div className="w-full flex flex-col flex-wrap md:flex-row md:justify-around mx-auto h-full py-12 items-stretch">
+        <div className="w-2/3 lg:1/3 xl:w-1/5 rounded-lg bg-paleYellow shadow-xl">
+          <div className="px-4 py-16 rounded ">
+            <p className="text-darkerBlue text-3xl mb-8">Easy Apply</p>
+            <p className=" text-base text-darkerBlue ">
+              {"You can apply easy by one click!"}
+            </p>
+          </div>
         </div>
-      </div>
-      
-      <div className="w-2/3 lg:1/3 xl:w-1/5 rounded-lg bg-darkP shadow-lg">
-        <div className="px-4 py-16 bg-paleYellow shadow rounded ">
-          <p className="text-darkerBlue text-3xl mb-8" >Guranteed </p>
-          <p className=" text-base text-darkerBlue ">
-            {(' with us you will absolutely find what you want and evreything is for guranteed!')}
-          </p>
-        </div>
-      </div>
 
-      <div className="w-2/3 lg:1/3 xl:w-1/5 rounded-lg bg-darkP shadow-lg">
-        <div className="px-4 py-16 bg-paleYellow shadow rounded ">
-          <p className="text-darkerBlue text-3xl mb-8" >Free</p>
-          <p className=" text-base text-darkerBlue ">
-            {('apply and be a part of us for free!')}
-          </p>
+        <div className="w-2/3 lg:1/3 xl:w-1/5 rounded-lg bg-paleYellow shadow-xl">
+          <div className="px-4 py-16 rounded ">
+            <p className="text-darkerBlue text-3xl mb-8">Guranteed </p>
+            <p className=" text-base text-darkerBlue ">
+              {
+                "With us you will absolutely find what you want and evreything is for guranteed!"
+              }
+            </p>
+          </div>
+        </div>
+
+        <div className="w-2/3 lg:1/3 xl:w-1/5 rounded-lg bg-paleYellow shadow-xl">
+          <div className="px-4 py-16 rounded ">
+            <p className="text-darkerBlue text-3xl mb-8">Free</p>
+            <p className=" text-base text-darkerBlue ">
+              {"Apply and be a part of us for free!"}
+            </p>
+          </div>
         </div>
       </div>
     </div>
-   
-        </div>
-    )
-}
+  );
+};
 
-export default WhyUs
+export default WhyUs;
